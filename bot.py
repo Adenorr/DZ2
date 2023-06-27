8b3d9314099e1509f9075d66968c11ecffd1991b
Adenorr <55825588+Adenorr@users.noreply.github.com>
2023-06-27 13:55:26 +0300

    dz2

---
+++ bot.py
@@ -0,0 +1,438 @@
+# extractive
+
+import os
+import sys
+import math
+import torch
+
+import telebot
+from telebot import types
+
+import re
+from rouge import Rouge
+import razdel
+from tqdm.notebook import tqdm
+
+from nltk.translate.bleu_score import corpus_bleu
+from rouge import Rouge
+
+def calc_scores(references, predictions, metric="all"):
+    print("Count:", len(predictions))
+    print("Ref:", references[-1])
+    print("Hyp:", predictions[-1])
+
+    if metric in ("bleu", "all"):
+        print("BLEU: ", corpus_bleu([[r] for r in references], predictions))
+    if metric in ("rouge", "all"):
+        rouge = Rouge()
+        scores = rouge.get_scores(predictions, references, avg=True)
+        print("ROUGE: ", scores)
+
+import copy
+
+def build_oracle_summary_greedy(text, gold_summary, calc_score, lower=True, max_sentences=30):
+    '''
+    Жадное построение oracle summary
+    '''
+    gold_summary = gold_summary.lower() if lower else gold_summary
+    # Делим текст на предложения
+    sentences = [sentence.text.lower() if lower else sentence.text for sentence in razdel.sentenize(text)][:max_sentences]
+    n_sentences = len(sentences)
+    oracle_summary_sentences = set()
+    
+    score = -1.0
+    summaries = []
+    for _ in range(n_sentences):
+        for i in range(n_sentences):
+            if i in oracle_summary_sentences:
+                continue
+            current_summary_sentences = copy.copy(oracle_summary_sentences)
+            # Добавляем какое-то предложения к уже существующему summary
+            current_summary_sentences.add(i)
+            current_summary = " ".join([sentences[index] for index in sorted(list(current_summary_sentences))])
+            # Считаем метрики
+            current_score = calc_score(current_summary, gold_summary)
+            summaries.append((current_score, current_summary_sentences))
+        # Если получилось улучшить метрики с добавлением какого-либо предложения, то пробуем добавить ещё
+        # Иначе на этом заканчиваем
+        best_summary_score, best_summary_sentences = max(summaries)
+        if best_summary_score <= score:
+            break
+        oracle_summary_sentences = best_summary_sentences
+        score = best_summary_score
+    oracle_summary = " ".join([sentences[index] for index in sorted(list(oracle_summary_sentences))])
+    return oracle_summary, oracle_summary_sentences
+
+def calc_single_score(pred_summary, gold_summary, rouge):
+    return rouge.get_scores([pred_summary], [gold_summary], avg=True)['rouge-2']['f']
+
+from tqdm.notebook import tqdm
+import razdel
+
+def calc_oracle_score(records, nrows=1000, lower=True):
+    references = []
+    predictions = []
+    rouge = Rouge()
+
+    for i, record in tqdm(enumerate(records)):
+        if i >= nrows:
+            break
+
+        summary = record["summary"]
+        summary = summary if not lower else summary.lower()
+        references.append(summary)
+
+        text = record["text"]
+        predicted_summary, _ = build_oracle_summary_greedy(text, summary, calc_score=lambda x, y: calc_single_score(x, y, rouge))
+        predictions.append(predicted_summary)
+
+    calc_scores(references, predictions)
+
+import youtokentome as yttm
+
+#def train_bpe(records, model_path, model_type="bpe", vocab_size=10000, lower=True):
+#    temp_file_name = "temp.txt"
+#    with open(temp_file_name, "w") as temp:
+#        for record in records:
+#            text, summary = record['text'], record['summary']
+#            if lower:
+#                summary = summary.lower()
+#                text = text.lower()
+#            if not text or not summary:
+#                continue
+#            temp.write(text + "\n")
+#            temp.write(summary + "\n")
+#    yttm.BPE.train(data=temp_file_name, vocab_size=vocab_size, model=model_path)
+
+from rouge import Rouge
+import razdel
+from tqdm.notebook import tqdm
+
+def add_oracle_summary_to_records(records, max_sentences=30, lower=True, nrows=1000):
+    rouge = Rouge()
+    for i, record in tqdm(enumerate(records)):
+        if i >= nrows:
+            break
+        print(record)
+        print( type(record) )
+
+        text = record["text"]
+#        summary = record["summary"]
+#        text = record
+        summary = 'summary'
+
+        summary = summary.lower() if lower else summary
+        sentences = [sentence.text.lower() if lower else sentence.text for sentence in razdel.sentenize(text)][:max_sentences]
+        oracle_summary, sentences_indicies = build_oracle_summary_greedy(text, summary, calc_score=lambda x, y: calc_single_score(x, y, rouge), lower=lower, max_sentences=max_sentences)
+        record["sentences"] = sentences
+        record["oracle_sentences"] = list(sentences_indicies)
+        record["oracle_summary"] = oracle_summary
+
+    return records[:nrows]
+
+import torch
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+import random
+import math
+import razdel
+import torch
+import numpy as np
+from rouge import Rouge
+
+
+class BatchIterator():
+    def __init__(self, records, vocabulary, batch_size, bpe_processor, shuffle=True, lower=True, max_sentences=30, max_sentence_length=50, device=torch.device('cpu')):
+        self.records = records
+        self.num_samples = len(records)
+        self.batch_size = batch_size
+        self.bpe_processor = bpe_processor
+        self.shuffle = shuffle
+        self.batches_count = int(math.ceil(self.num_samples / batch_size))
+        self.lower = lower
+        self.rouge = Rouge()
+        self.vocabulary = vocabulary
+        self.max_sentences = max_sentences
+        self.max_sentence_length = max_sentence_length
+        self.device = device
+
+    def __len__(self):
+        return self.batches_count
+
+    def __iter__(self):
+        print(self.records)
+        print(self.num_samples)
+
+        indices = np.arange(self.num_samples)
+        print(indices)
+
+        if self.shuffle:
+            np.random.shuffle(indices)
+
+        for start in range(0, self.num_samples, self.batch_size):
+            end = min(start + self.batch_size, self.num_samples)
+            batch_indices = indices[start:end]
+
+            batch_inputs = []
+            batch_outputs = []
+            max_sentence_length = 0
+            max_sentences = 0
+            batch_records = []
+
+            for data_ind in batch_indices:
+                print(data_ind)
+
+                record = self.records[data_ind]
+                batch_records.append(record)
+
+                print(record)
+                print(type(record))
+                text = record["text"]
+                summary = record["summary"]
+                summary = summary.lower() if self.lower else summary
+
+                if "sentences" not in record:
+                    sentences = [sentence.text.lower() if self.lower else sentence.text for sentence in razdel.sentenize(text)][:self.max_sentences]
+                else:
+                    sentences = record["sentences"]
+                max_sentences = max(len(sentences), max_sentences)
+
+                # номера предложений, которые в нашем саммари
+                if "oracle_sentences" not in record:
+                    calc_score = lambda x, y: calc_single_score(x, y, self.rouge)
+                    sentences_indicies = build_oracle_summary_greedy(text, summary, calc_score=calc_score, lower=self.lower, max_sentences=self.max_sentences)[1]
+                else:
+                    sentences_indicies = record["oracle_sentences"]
+
+                # inputs - индексы слов в предложении
+                inputs = [self.bpe_processor.encode(sentence)[:self.max_sentence_length] for sentence in sentences]
+                max_sentence_length = max(max_sentence_length, max([len(tokens) for tokens in inputs]))
+
+                # получение метки класса предложения
+                outputs = [int(i in sentences_indicies) for i in range(len(sentences))]
+                batch_inputs.append(inputs)
+                batch_outputs.append(outputs)
+
+            tensor_inputs = torch.zeros((self.batch_size, max_sentences, max_sentence_length), dtype=torch.long, device=self.device)
+            tensor_outputs = torch.zeros((self.batch_size, max_sentences), dtype=torch.float32, device=self.device)
+
+
+            for i, inputs in enumerate(batch_inputs):
+                for j, sentence_tokens in enumerate(inputs):
+                    tensor_inputs[i][j][:len(sentence_tokens)] = torch.LongTensor(sentence_tokens)
+
+            for i, outputs in enumerate(batch_outputs):
+                tensor_outputs[i][:len(outputs)] = torch.LongTensor(outputs)
+
+            yield {
+                'inputs': tensor_inputs,
+                'outputs': tensor_outputs,
+                'records': batch_records
+            }
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+
+from torch.nn.utils.rnn import pack_padded_sequence as pack
+from torch.nn.utils.rnn import pad_packed_sequence as unpack
+
+class SentenceEncoderRNN(nn.Module):
+    def __init__(self, input_size, embedding_dim, hidden_size, n_layers=3, dropout=0.3, bidirectional=True):
+        super().__init__()
+
+        num_directions = 2 if bidirectional else 1
+        assert hidden_size % num_directions == 0
+        hidden_size = hidden_size // num_directions
+
+        self.embedding_dim = embedding_dim
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+
+        self.embedding_layer = nn.Embedding(input_size, embedding_dim)
+        self.rnn_layer = nn.LSTM(embedding_dim, hidden_size, n_layers, dropout=dropout, bidirectional=bidirectional, batch_first=True)
+        self.dropout_layer = nn.Dropout(dropout)
+
+    def forward(self, inputs, hidden=None):
+        embedded = self.embedding_layer(inputs)
+        outputs, _ = self.rnn_layer(embedded, hidden)
+        sentences_embeddings = torch.mean(outputs, 1)
+        # [batch_size, hidden_size]
+        return sentences_embeddings
+
+class SentenceTaggerRNN(nn.Module):
+    def __init__(self,
+                 vocabulary_size,
+                 token_embedding_dim=256,
+                 sentence_encoder_hidden_size=256,
+                 hidden_size=256,
+                 bidirectional=True,
+                 sentence_encoder_n_layers=2,
+                 sentence_encoder_dropout=0.3,
+                 sentence_encoder_bidirectional=True,
+                 n_layers=1,
+                 dropout=0.3):
+        
+        super().__init__()
+
+        num_directions = 2 if bidirectional else 1
+        assert hidden_size % num_directions == 0
+        hidden_size = hidden_size // num_directions
+
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+
+        self.sentence_encoder = SentenceEncoderRNN(vocabulary_size, token_embedding_dim,
+                                                   sentence_encoder_hidden_size, sentence_encoder_n_layers, 
+                                                   sentence_encoder_dropout, sentence_encoder_bidirectional)
+        
+        self.rnn_layer = nn.LSTM(sentence_encoder_hidden_size, hidden_size, n_layers, dropout=dropout,
+                           bidirectional=bidirectional, batch_first=True)
+        
+        self.dropout_layer = nn.Dropout(dropout)
+        self.content_linear_layer = nn.Linear(hidden_size * 2, 1)
+        self.document_linear_layer = nn.Linear(hidden_size * 2, hidden_size * 2)
+        self.salience_linear_layer = nn.Linear(hidden_size * 2, hidden_size * 2)
+        self.tanh_layer = nn.Tanh()
+
+    def forward(self, inputs, hidden=None):
+
+        # [batch_size, seq num, seq_len]
+        batch_size = inputs.size(0)
+        sentences_count = inputs.size(1)
+        tokens_count = inputs.size(2)
+        inputs = inputs.reshape(-1, tokens_count)
+        # [batch_size * seq num, seq_len]
+
+        embedded_sentences = self.sentence_encoder(inputs)
+        embedded_sentences = embedded_sentences.reshape(batch_size, sentences_count, -1)
+        # [batch_size *  seq num, seq_len, hidden_size] -> [batch_size, seq num, hidden_size]
+
+        outputs, _ = self.rnn_layer(embedded_sentences, hidden)
+        outputs = self.dropout_layer(outputs)
+        # [batch_size, seq num, hidden_size]
+
+        document_embedding = self.tanh_layer(self.document_linear_layer(torch.mean(outputs, 1)))
+        # [batch_size, hidden_size]
+
+        # W * h^T
+        content = self.content_linear_layer(outputs).squeeze(2) # 1-representation
+        # [batch_size, seq num]
+
+        # h^T * W * d
+        salience = torch.bmm(outputs, self.salience_linear_layer(document_embedding).unsqueeze(2)).squeeze(2) # 2-representation
+
+        # [batch_size, seq num, hidden_size] * [batch_size, hidden_size, 1] = [batch_size, seq num, ]
+        return content + salience
+
+import re
+def punct_detokenize(text):
+    text = text.strip()
+    punctuation = ",.!?:;%"
+    closing_punctuation = ")]}"
+    opening_punctuation = "([}"
+    for ch in punctuation + closing_punctuation:
+        text = text.replace(" " + ch, ch)
+    for ch in opening_punctuation:
+        text = text.replace(ch + " ", ch)
+    res = [r'"\s[^"]+\s"', r"'\s[^']+\s'"]
+    for r in res:
+        for f in re.findall(r, text, re.U):
+            text = text.replace(f, f[0] + f[2:-2] + f[-1])
+    text = text.replace("' s", "'s").replace(" 's", "'s")
+    text = text.strip()
+    return text
+
+
+def postprocess(ref, hyp, is_multiple_ref=False, detokenize_after=False, tokenize_after=True):
+    if is_multiple_ref:
+        reference_sents = ref.split(" s_s ")
+        decoded_sents = hyp.split("s_s")
+        hyp = [w.replace("<", "&lt;").replace(">", "&gt;").strip() for w in decoded_sents]
+        ref = [w.replace("<", "&lt;").replace(">", "&gt;").strip() for w in reference_sents]
+        hyp = " ".join(hyp)
+        ref = " ".join(ref)
+    ref = ref.strip()
+    hyp = hyp.strip()
+    if detokenize_after:
+        hyp = punct_detokenize(hyp)
+        ref = punct_detokenize(ref)
+    if tokenize_after:
+        hyp = hyp.replace("@@UNKNOWN@@", "<unk>")
+        hyp = " ".join([token.text for token in razdel.tokenize(hyp)])
+        ref = " ".join([token.text for token in razdel.tokenize(ref)])
+    return ref, hyp
+
+from rouge import Rouge
+import razdel
+#def prepare_data(text, max_sentences=30, lower=True, nrows=1000):
+#    rouge = Rouge()
+#    record = {}
+#
+#    sentences = [sentence.text.lower() if lower else sentence.text for sentence in razdel.sentenize(text)][:max_sentences]
+#
+#    record["sentences"] = sentences[:nrows]
+#    record["oracle_sentences"] = ""
+#    record["oracle_summary"] = ""
+#
+#    return record
+
+
+#bot = telebot.TeleBot('6081910771:AAGjwKiSrrlTwmYNQKijye2GqUb_GrFuPqI')
+bot = telebot.TeleBot('6272218401:AAH15dvLYeZYPVXN9Uw9oaH7W5zOUuA32Yc')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, ''.join(['🐧, ', message.from_user.first_name]))
+
+
+@bot.message_handler(commands=['help'])
+def help(message):
+    bot.reply_to(message,
+                 'Отправь новостное сообщение и получи его краткое содержание.\n',
+                 parse_mode='Markdown')
+
+@bot.message_handler(func=lambda m: not m.text.startswith('/'), content_types=['text'])
+def func(message):
+    inputs = [{"text":message.text, 'summary':''}]
+
+    new_ext_test_records = add_oracle_summary_to_records(inputs, nrows=256)
+
+#    new_ext_test_records = prepare_data(inputs, nrows=256)
+    print(new_ext_test_records)
+
+    bpe_processor = yttm.BPE('BPE_model.bin')
+    vocabulary = bpe_processor.vocab()
+
+    test_iterator = BatchIterator(new_ext_test_records, vocabulary, 32, bpe_processor, device=device)
+
+    model = SentenceTaggerRNN(len(vocabulary))
+    model = torch.load('summary.pt', map_location=torch.device('cpu'))
+
+    pred_summary = ''
+
+    top_k = 3
+    for batch in test_iterator:
+        print('start')
+        logits = model(batch['inputs'])
+        sum_in = torch.argsort(logits, dim=1)[:, -top_k:]
+        for i in range(len(batch['outputs'])):
+            print('pred')
+            pred_summary = ' '.join([batch['records'][i]['sentences'][ind] for ind in sum_in.sort(dim=1)[0][i]])
+            print(pred_summary)
+            break
+        
+    print(pred_summary)
+
+    bot.send_message(message.from_user.id, pred_summary, reply_to_message_id=message.message_id)
+
+bot.polling(none_stop=True, interval=0)
